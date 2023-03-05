f1411acfb4b1695733d9e64edd7c55b6208ec5be
julianpodruhin <yulianpodruhin@mail.ru>
2023-03-05 19:30:12 +0300

    Lab 4 ex 5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.lang.*;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        int array[] = new int[10];
+        if (array[0] == 3 || array[9]==3);
+        {
+            System.out.println("True");
+        }
     }
 }
